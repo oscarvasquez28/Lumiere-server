0d4b9aa5a4ac692f14b4d55fffe471528f53f3c5
Óscar Fabián Vásquez Soto <oscar.vasquezot@uanl.edu.mx>
2024-11-09 01:19:21 -0600

    Add getPostByUserId and updatePostStatus functions with corresponding routes

--- models/postModel.js
+++ models/postModel.js
@@ -29,3 +29,28 @@ export const createPost = (postData, callback) => {
     const sql = 'INSERT INTO posts (title, user_id, category_id, image) VALUES (?, ?, ?, ?)';
     db.query(sql, [postData.title, postData.user_id, postData.category_id, imageBuffer], callback);
 };
+
+export const getPostByUserId = (userId, callback) => {
+    const sql = 'SELECT * FROM posts WHERE user_id = ?';
+    db.query(sql, [userId], (error, results) => {
+        if (error) return callback(error);
+
+        if (results.length > 0) {
+            // Iterar sobre cada post y convertir la imagen a Base64
+            results.forEach(post => {
+                if (post.image) {
+                    post.image = post.image.toString('base64');
+                }
+            });
+            callback(null, results);
+        } else {
+            callback(new Error("No se encontraron posts"));
+        }
+    });
+};
+
+export const updatePostStatus = (postData, callback) => {
+    const sql = 'UPDATE posts SET status = ? WHERE id = ?';
+    db.query(sql, [postData.status, postData.id], callback);
+}
+
